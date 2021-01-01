9ef4e4c0be5ae4606a68cc97e364e6213fbde222
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2021-01-01 19:53:11 +0000

    add source to links_to_html.lua
    ...because I didn't write this myself

--- bin/links_to_html.lua
+++ bin/links_to_html.lua
@@ -1,3 +1,4 @@
+-- https://web.archive.org/web/20210101195140/https://stackoverflow.com/questions/40993488/convert-markdown-links-to-html-with-pandoc
 function Link(el)
   el.target = string.gsub(el.target, "%.md", ".html")
   return el
