5a19887496bcf1f723e861b3d845bb182095feb7
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2021-05-29 18:37:56 +0100

    implement srs q&a prompt filter for pandoc

--- bin/srs_qna_prompts.py
+++ bin/srs_qna_prompts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from pandocfilters import toJSONFilter, Para
+from pandocfilters import Div, Plain, toJSONFilter, attributes
 
 """
 Convert any 'question' and 'answer' text into text that spans
@@ -17,7 +17,39 @@ look fine in Markdown)
 
 def reformat_spaced_repetition_text(key, value, format, meta):
   if key == 'Para':
-    print('hello world')
+    # the spaced repetition text will always start with 'Q:'
+    first_word = value[0]['c']
+    if first_word == 'Q:':
+      # the content of a paragraph is a list of inline elements
+      # e.g. links, line breaks, in-line code snippets, text
+      started_parsing_answer: bool = False
+
+      para_question = []
+      para_answer = []
+
+      for inline in value:
+        # there's always a soft line break before the 'A:'
+        # once we've seen the soft break, we want to start populating
+        # the list containing the inlines that make up the answer
+        if inline['t'] == 'SoftBreak':
+          started_parsing_answer = True
+        
+        if started_parsing_answer:
+          para_answer.append(inline)
+        else:
+          para_question.append(inline)
+        
+      # see constructor doc: https://github.com/jgm/pandocfilters/blob/master/pandocfilters.py
+      return [
+        Div(
+          attributes({'class': 'question'}),
+          [Plain(para_question)]
+        ),
+        Div(
+          attributes({'class': 'answer'}),
+          [Plain(para_answer)]
+        )
+      ]
 
 if __name__ == "__main__":
   toJSONFilter(reformat_spaced_repetition_text)
