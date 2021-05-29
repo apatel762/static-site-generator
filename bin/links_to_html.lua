322c433773215b83512770f037314eb10644e78b
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2021-05-29 16:39:12 +0100

    rewrite links_to_html in python
    so that it's easier to maintain it in the long run

--- bin/links_to_html.lua
+++
@@ -1,5 +0,0 @@
--- https://web.archive.org/web/20210101195140/https://stackoverflow.com/questions/40993488/convert-markdown-links-to-html-with-pandoc
-function Link(el)
-  el.target = string.gsub(el.target, "%.md", ".html")
-  return el
-end
